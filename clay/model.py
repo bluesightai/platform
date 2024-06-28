b68724bd06485618ed67f1be6ab484f032c1928a
Alexander Smirnov <alex@asmirnov.xyz>
2024-06-28 19:37:06 +0100

    add clay model

---
+++ clay/model.py
@@ -0,0 +1,248 @@
+import math
+import os
+import re
+
+import torch
+from args import args, device
+from einops import rearrange, repeat
+from factory import DynamicEmbedding
+from huggingface_hub import hf_hub_download
+from loguru import logger
+from torch import nn
+from utils import get_mock_data, posemb_sincos_2d_with_gsd, stack_to_datacube
+from vit_pytorch.simple_vit import Transformer
+
+torch.set_float32_matmul_precision("medium")
+os.environ["TORCH_CUDNN_V8_API_DISABLED"] = "1"
+
+
+class Encoder(nn.Module):
+    def __init__(  # noqa: PLR0913
+        self,
+        mask_ratio=0.0,
+        patch_size=8,
+        shuffle=False,
+        dim=768,
+        depth=12,
+        heads=12,
+        dim_head=64,
+        mlp_ratio=4.0,
+    ):
+        super().__init__()
+        self.mask_ratio = mask_ratio
+        self.patch_size = patch_size
+        self.shuffle = shuffle
+        self.dim = dim
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim) * 0.02)
+
+        self.patch_embedding = DynamicEmbedding(
+            wave_dim=128,
+            num_latent_tokens=128,
+            patch_size=patch_size,
+            embed_dim=dim,
+            is_decoder=False,
+        )
+
+        self.transformer = Transformer(
+            dim=dim,
+            depth=depth,
+            heads=heads,
+            dim_head=dim_head,
+            mlp_dim=int(dim * mlp_ratio),
+        )
+
+    def to_patch_embed(self, cube, waves):
+        """Split the input cube into patches & create embeddings per patch"""
+        patches, waves_encoded = self.patch_embedding(cube, waves)  # [B L D]
+        return patches, waves_encoded  # ([B L D], [N D])
+
+    def add_encodings(self, patches, time, latlon, gsd):
+        """Add position encoding to the patches"""
+        B, L, D = patches.shape
+
+        grid_size = int(math.sqrt(L))
+        self.num_patches = grid_size**2
+
+        pos_encoding = (
+            posemb_sincos_2d_with_gsd(
+                h=grid_size,
+                w=grid_size,
+                dim=(self.dim - 8),
+                gsd=gsd,
+            )
+            .to(patches.device)
+            .detach()
+        )  # [L (D - 8)]
+        logger.info(" pos_encoding is", pos_encoding.shape, "It should be  [L (D - 8)]")
+
+        time_latlon = torch.hstack((time, latlon)).to(patches.device).detach()  # [B 8]
+        logger.info("time_latlon is", time_latlon.shape, "It should be  [B 8]")
+
+        pos_encoding = repeat(pos_encoding, "L D -> B L D", B=B)  # [B L (D - 8)]
+        logger.info("repeated pos_encoding is", pos_encoding.shape, "It should be  [B L (D - 8)]")
+
+        time_latlon = repeat(time_latlon, "B D -> B L D", L=L)  # [B L 8]
+        logger.info("repeated time_latlon is", time_latlon.shape, "It should be  [B L 8]")
+        pos_metadata_encoding = torch.cat((pos_encoding, time_latlon), dim=-1)  # [B L D]
+        logger.info("JUST BEFORE ADDING, PATCHES ARE", patches.shape, "METADATA", pos_metadata_encoding.shape)
+
+        patches = patches + pos_metadata_encoding  # [B L D] + [B L D] -> [B L D]
+        return patches  # [B L D]
+
+    def mask_out(self, patches):
+        """
+        Mask out patches randomly by shuffling the patches & masking out the
+        first N patches
+
+        Parameters
+        ----------
+        patches : torch.Tensor A tensor of shape (B, L, D)
+
+        Returns
+        -------
+        unmasked_patches : torch.Tensor
+            A tensor of shape (B, L:(1 - mask_ratio), D) containing the
+            embeddings of the unmasked patches.
+        unmasked_indices : torch.Tensor
+            A tensor of shape (B, (1 - mask_ratio)) containing the indices of
+            the unmasked patches.
+        masked_indices : torch.Tensor
+            A tensor of shape (B, mask_ratio) containing the indices of the
+            masked patches.
+        masked_matrix : torch.Tensor
+            A tensor of shape (B, L) containing the mask matrix, 1 indicates a masked
+            patch & 0 indicates an unmasked patch.
+        """
+        B, L, D = patches.shape
+        # assert (
+        #     L == self.num_patches
+        # ), f"Expected {self.num_patches} patches, got {L} patches."
+
+        if self.shuffle:  # Shuffle the patches
+            noise = torch.randn((B, L), device=patches.device)  # [B L]
+        else:  # Don't shuffle, useful for interpolation & inspection of embeddings
+            noise = rearrange(torch.arange(B * L, device=patches.device), "(B L) -> B L", B=B, L=L)
+
+        random_indices = torch.argsort(noise, dim=-1)  # [B L]
+        reverse_indices = torch.argsort(random_indices, dim=-1)  # [B L]
+
+        num_masked_patches = int(self.mask_ratio * self.num_patches)  # Number of patches to be masked out
+        masked_indices, unmasked_indices = (
+            random_indices[:, :num_masked_patches],  # [B mask_ratio * L]
+            random_indices[:, num_masked_patches:],  # [B (1 - mask_ratio) * L]
+        )
+
+        # create a mask of shape B L, where 1 indicates a masked patch
+        # and 0 indicates an unmasked patch
+        masked_matrix = torch.zeros((B, L), device=patches.device)  # [B L] = 0
+        masked_matrix[:, :num_masked_patches] = 1  # [B mask_ratio * L] = 1
+        masked_matrix = torch.gather(
+            masked_matrix, dim=1, index=reverse_indices
+        )  # [B L] -> [B L] - reorder the patches
+
+        # mask out the patches
+        batch_indices = rearrange(torch.arange(B, device=patches.device), "B -> B 1")  # [B 1]
+        unmasked_patches = patches[batch_indices, unmasked_indices, :]  # [B L:(1 - mask_ratio) D]
+        _ = patches[batch_indices, masked_indices, :]  # [B L:mask_ratio D]
+
+        return (
+            unmasked_patches,
+            unmasked_indices,
+            masked_indices,
+            masked_matrix,
+        )  # [B L:(1 - mask_ratio) D], [(1-mask_ratio)], [mask_ratio], [B L]
+
+    def forward(self, datacube):
+        cube, time, latlon, gsd, waves = (
+            datacube["pixels"],  # [B C H W]
+            datacube["time"],  # [B 2]
+            datacube["latlon"],  # [B 2]
+            datacube["gsd"],  # 1
+            datacube["waves"],  # [N]
+        )  # [B C H W]
+
+        B, C, H, W = cube.shape
+
+        patches, waves_encoded = self.to_patch_embed(cube, waves)  # [B L D] - patchify & create embeddings per patch
+
+        patches = self.add_encodings(
+            patches,
+            time,
+            latlon,
+            gsd,
+        )  # [B L D] - add position encoding to the embeddings
+
+        # mask out patches
+        (
+            unmasked_patches,
+            unmasked_indices,
+            masked_indices,
+            masked_matrix,
+        ) = self.mask_out(
+            patches
+        )  # [B L:(1 - mask_ratio) D], [(1-mask_ratio)], [mask_ratio], [B L]
+
+        # Add class tokens
+        cls_tokens = repeat(self.cls_token, "1 1 D -> B 1 D", B=B)  # [B 1 D]
+        unmasked_patches = torch.cat((cls_tokens, unmasked_patches), dim=1)  # [B (1 + L) D]
+
+        # pass the unmasked patches through the transformer
+        encoded_unmasked_patches = self.transformer(unmasked_patches)  # [B ((1 + L)):(1 - mask_ratio)) D]
+
+        return (
+            encoded_unmasked_patches,
+            unmasked_indices,
+            masked_indices,
+            masked_matrix,
+        )  # [B ((1 + L):(1 - mask_ratio)) D], [(1-mask_ratio)], [mask_ratio], [B L]
+
+
+def get_encoder() -> Encoder:
+
+    ckpt_path = hf_hub_download(repo_id=args.hub_repo_id, filename=args.hub_filename)
+    encoder = Encoder()
+
+    ckpt = torch.load(ckpt_path, map_location=device)
+    state_dict = ckpt.get("state_dict")
+
+    # Remove model.encoder prefix for the clay encoder
+    state_dict = {
+        re.sub(r"^model\.encoder\.", "", name): param
+        for name, param in state_dict.items()
+        if name.startswith("model.encoder")
+    }
+
+    # Copy the weights from the state dict to the encoder
+    for name, param in encoder.named_parameters():
+        if name in state_dict and param.size() == state_dict[name].size():
+            param.data.copy_(state_dict[name])  # Copy the weights
+        else:
+            logger.warning(f"No matching parameter for {name} with size {param.size()}")
+
+    # Freeze clay encoder
+    for param in encoder.parameters():
+        param.requires_grad = False
+
+    # Set the encoder to evaluation mode
+    encoder.eval()
+    encoder = encoder.to(device)
+    return encoder
+
+
+if __name__ == "__main__":
+
+    encoder = get_encoder()
+
+    stack, lat, long = get_mock_data()
+    datacube = stack_to_datacube(stack, lat, long)
+
+    logger.info(stack)
+
+    with torch.no_grad():
+        unmsk_patch, unmsk_idx, msk_idx, msk_matrix = encoder(datacube)
+
+    # The first embedding is the class token, which is the
+    # overall single embedding. We extract that for PCA below.
+    embeddings = unmsk_patch[:, 0, :].cpu().numpy()
+
+    logger.info(embeddings)
---
+++ clay/utils.py
@@ -0,0 +1,192 @@
+"""
+Code from https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/simple_vit.py
+
+"""
+
+import math
+from typing import Any, Dict, Tuple
+
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import pystac_client
+import stackstac
+import torch
+import torch.nn.functional as F
+from args import args, device, metadata
+from rasterio.enums import Resampling
+from shapely import Point
+from torchvision.transforms import v2
+from xarray import DataArray
+
+
+def posemb_sincos_2d(h, w, dim, temperature: int = 10000, dtype=torch.float32):
+    y, x = torch.meshgrid(torch.arange(h), torch.arange(w), indexing="ij")
+    assert (dim % 4) == 0, "feature dimension must be multiple of 4 for sincos emb"
+    omega = torch.arange(dim // 4) / (dim // 4 - 1)
+    omega = 1.0 / (temperature**omega)
+
+    y = y.flatten()[:, None] * omega[None, :]
+    x = x.flatten()[:, None] * omega[None, :]
+    pe = torch.cat((x.sin(), x.cos(), y.sin(), y.cos()), dim=1)
+    return pe.type(dtype)
+
+
+def posemb_sincos_2d_with_gsd(h, w, dim, gsd=1.0, temperature: int = 10000, dtype=torch.float32):
+    y, x = torch.meshgrid(torch.arange(h), torch.arange(w), indexing="ij")
+    assert (dim % 4) == 0, "feature dimension must be multiple of 4 for sincos emb"
+
+    omega = torch.arange(dim // 4) / (dim // 4 - 1)
+    omega = 1.0 / (temperature ** (2 * omega / dim)) * (gsd / 1.0)  # Adjusted for g
+
+    y = y.flatten()[:, None] * omega[None, :]
+    x = x.flatten()[:, None] * omega[None, :]
+    pe = torch.cat((x.sin(), x.cos(), y.sin(), y.cos()), dim=1)
+    return pe.type(dtype)
+
+
+def posemb_sincos_1d(pos, dim, temperature: int = 10000, dtype=torch.float32):
+    assert dim % 2 == 0, "Feature dimension must be a multiple of 2 for sincos embedding"
+    pos = torch.arange(pos) if isinstance(pos, int) else pos
+
+    omega = torch.arange(dim // 2) / (dim // 2 - 1)
+    omega = 1.0 / (temperature**omega)
+
+    scaled_pos = pos[:, None] * omega[None, :]
+    pe = torch.cat((scaled_pos.sin(), scaled_pos.cos()), dim=1)
+
+    return pe.type(dtype)
+
+
+# Prep datetimes embedding using a normalization function from the model code.
+def normalize_timestamp(date):
+    week = date.isocalendar().week * 2 * np.pi / 52
+    hour = date.hour * 2 * np.pi / 24
+
+    return (math.sin(week), math.cos(week)), (math.sin(hour), math.cos(hour))
+
+
+# Prep lat/lon embedding using the
+def normalize_latlon(lat, lon):
+    lat = lat * np.pi / 180
+    lon = lon * np.pi / 180
+    return (math.sin(lat), math.cos(lat)), (math.sin(lon), math.cos(lon))
+
+
+def get_mock_data() -> Tuple[DataArray, float, float]:
+    # Point over Monchique Portugal
+    lat, lon = 37.30939, -8.57207
+
+    # Dates of a large forest fire
+    start = "2018-07-01"
+    end = "2018-09-01"
+
+    STAC_API = "https://earth-search.aws.element84.com/v1"
+    COLLECTION = "sentinel-2-l2a"
+
+    # Search the catalogue
+    catalog = pystac_client.Client.open(STAC_API)
+    search = catalog.search(
+        collections=[COLLECTION],
+        datetime=f"{start}/{end}",
+        bbox=(lon - 1e-5, lat - 1e-5, lon + 1e-5, lat + 1e-5),
+        max_items=100,
+        query={"eo:cloud_cover": {"lt": 80}},
+    )
+
+    all_items = search.get_all_items()
+
+    # Reduce to one per date (there might be some duplicates
+    # based on the location)
+    items = []
+    dates = []
+    for item in all_items:
+        if item.datetime.date() not in dates:
+            items.append(item)
+            dates.append(item.datetime.date())
+
+    # Extract coordinate system from first item
+    epsg = items[0].properties["proj:epsg"]
+
+    # Convert point of interest into the image projection
+    # (assumes all images are in the same projection)
+    poidf = gpd.GeoDataFrame(
+        pd.DataFrame(),
+        crs="EPSG:4326",
+        geometry=[Point(lon, lat)],
+    ).to_crs(epsg)
+
+    coords = poidf.iloc[0].geometry.coords[0]
+
+    # Create bounds in projection
+    size = 256
+    gsd = 10
+    bounds = (
+        coords[0] - (size * gsd) // 2,
+        coords[1] - (size * gsd) // 2,
+        coords[0] + (size * gsd) // 2,
+        coords[1] + (size * gsd) // 2,
+    )
+
+    stack = stackstac.stack(
+        items,
+        bounds=bounds,
+        snap_bounds=False,
+        epsg=epsg,
+        resolution=gsd,
+        dtype=np.dtype("float32"),
+        rescale=False,
+        fill_value=0,
+        assets=["blue", "green", "red", "nir"],
+        resampling=Resampling.nearest,
+    )
+
+    stack = stack.compute()
+
+    return stack, lat, lon
+
+
+def stack_to_datacube(stack: DataArray, lat: float, long: float) -> Dict[str, Any]:
+
+    mean = []
+    std = []
+    waves = []
+    for band in stack.band:
+        mean.append(metadata[args.platform].bands.mean[str(band.values)])
+        std.append(metadata[args.platform].bands.std[str(band.values)])
+        waves.append(metadata[args.platform].bands.wavelength[str(band.values)])
+
+    # Prepare the normalization transform function using the mean and std values.
+    transform = v2.Compose(
+        [
+            v2.Normalize(mean=mean, std=std),
+        ]
+    )
+
+    datetimes = stack.time.values.astype("datetime64[s]").tolist()
+    times = [normalize_timestamp(dat) for dat in datetimes]
+    week_norm = [dat[0] for dat in times]
+    hour_norm = [dat[1] for dat in times]
+
+    latlons = [normalize_latlon(lat, long)] * len(times)
+    lat_norm = [dat[0] for dat in latlons]
+    lon_norm = [dat[1] for dat in latlons]
+
+    # Normalize pixels
+    pixels = torch.from_numpy(stack.data.astype(np.float32))
+    pixels = transform(pixels)
+
+    datacube: Dict[str, Any] = {
+        "platform": args.platform,
+        "time": torch.tensor(
+            np.hstack((week_norm, hour_norm)),
+            dtype=torch.float32,
+            device=device,
+        ),
+        "latlon": torch.tensor(np.hstack((lat_norm, lon_norm)), dtype=torch.float32, device=device),
+        "pixels": pixels.to(device),
+        "gsd": torch.tensor(stack.gsd.values, device=device),
+        "waves": torch.tensor(waves, device=device),
+    }
+
+    return datacube
