648eb79205b7e60717191d4b21c4eee3d0a9403c
Alexander Smirnov <alex@asmirnov.xyz>
2024-06-29 15:08:47 +0100

    upd malevich flow

--- clay/malevich/flows.py
+++ clay/malevich/flows.py
@@ -1,17 +1,20 @@
 from malevich import Core, flow
-from malevich.clay import inference
-
-from clay.utils import get_mock_data
+from malevich.clay import get_malevich_mock_data, inference
 
 
 @flow(reverse_id="inference")
 def inference_flow():
-    _, inputs = get_mock_data()
+    inputs = get_malevich_mock_data()
     embeddings = inference(inputs)
     return embeddings
 
 
-task = Core(inference_flow, core_host="https://core.malevich.ai/", user="furiousteabag@gmail.com")
+task = Core(
+    inference_flow,
+    core_host="https://core.malevich.ai/",
+    user="c148403e41894c74a2d83f0703d1223c",
+    access_key="40f191df-d8ce-483a-a5ce-8a654c1f19bd",
+)
 task.prepare()
 task.run()
 print(task.results()[0].get_df())
