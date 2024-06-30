0874d7f3eaf0c0919e7984d7f19b4d4b4c36507c
Alexander Smirnov <alex@asmirnov.xyz>
2024-06-30 13:24:33 +0100

    upd malevich bindings

--- clay/malevich/bindings.py
+++ clay/malevich/bindings.py
@@ -56,20 +56,6 @@ def inference(inputs: MalevichInputEarthData, context: Context):
     datacube = stack_to_datacube(stack, inputs.lat, inputs.long)
     with torch.no_grad():
         unmsk_patch, unmsk_idx, msk_idx, msk_matrix = context.model(datacube)
-    embeddings = unmsk_patch[:, 0, :].cpu().numpy()
-    return embeddings
-
-
-@processor()
-def mock_inference(inputs: DF[InputEarthData], context: Context):
-    name_of_vectors = context.app_cfg.get("name", "clay_embeddings")
-
-    outputs = torch.rand(len(inputs), 768)
-    outputs = [repr(o.cpu().tolist()) for o in outputs]
-    return pd.DataFrame(outputs, columns=[name_of_vectors])
-
-
-@processor()
-def write_asset_file(just_input: DF, context: Context):
-    with open(context.get_object("example_file.txt").path) as f:
-        return pd.DataFrame([f.read()], columns=["contents"])
+    # embeddings = unmsk_patch[:, 0, :].cpu().numpy()
+    embeddings = unmsk_patch[:, 0, :].cpu().tolist()  # 1x768
+    return embeddings, coordinate_pairs
