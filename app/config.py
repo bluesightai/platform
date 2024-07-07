7499bf18f7f3f7ba0ad322515d75a262127ddfae
Alexander Smirnov <alex@asmirnov.xyz>
2024-07-07 19:38:03 +0100

    add fastapi config

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import os
+
+from pydantic_settings import BaseSettings
+from supabase import AClient, Client, create_client
+
+
+class Config(BaseSettings):
+
+    SUPABASE_URL: str = os.environ.get("SUPABASE_URL") or ""
+    SUPABASE_KEY: str = os.environ.get("SUPABASE_KEY") or ""
+
+
+config = Config()
+
+supabase: Client = create_client(config.SUPABASE_URL, config.SUPABASE_KEY)
