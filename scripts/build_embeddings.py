b554e3f4c33bc96df42687fa0663b719317bebd0
Alexander Smirnov <alex@asmirnov.xyz>
2024-07-07 19:30:27 +0100

    write intermediat to disk while build imbeddings

--- scripts/build_embeddings.py
+++ scripts/build_embeddings.py
@@ -5,7 +5,7 @@ import fire
 from loguru import logger
 from tqdm import tqdm
 
-from clay.args import args
+from clay.config import config
 from clay.model import get_embedding
 from clay.utils import get_bbox, get_square_centers
 
@@ -20,13 +20,25 @@ def process_center(lat: float, lon: float, size: int, gsd: int, start: str):
 
 
 def build_embeddings(
-    nw_lat: float, nw_lon: float, se_lat: float, se_lon: float, size: int, gsd: int, shift_percentage: float, start: str
+    nw_lat: float,
+    nw_lon: float,
+    se_lat: float,
+    se_lon: float,
+    size: int,
+    gsd: int,
+    shift_percentage: float,
+    start: str,
+    max_items_per_file: int = 5000,
 ):
     """
-    python scripts/build_embeddings.py 48.43996086356826 -101.38773653662064 48.3931714959406 -101.30094856808536 128 0.6 1.0 "2022-01-01"
+    north dakota
+    python scripts/build_embeddings.py 48.43996086356826 -101.38773653662064 48.3931714959406 -101.30094856808536 128 0.6 1.0 '2022-01-01'
+
+    nevada
+    python scripts/build_embeddings.py 36.25079152886272 -115.05193360689421 36.21906455965353, -115.01519546690653 128 0.6 1.0 '2022-01-01'
     """
 
-    filename = f"{nw_lat}:{nw_lon}|{se_lat}:{se_lon}|{args.platform}|{size}|{shift_percentage}|{gsd}.pkl"
+    base_filename = f"{nw_lat}:{nw_lon}|{se_lat}:{se_lon}|{config.platform}|{size}|{shift_percentage}|{gsd}.pkl"
     pixel_shift = int(shift_percentage * size)
 
     centers = get_square_centers(
@@ -34,7 +46,7 @@ def build_embeddings(
     )
     logger.info(f"Will be creating {len(centers)} points!")
 
-    data = []
+    data, idx = [], 0
     with tqdm(total=len(centers)) as pbar:
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = {
@@ -48,10 +60,18 @@ def build_embeddings(
                     data.append(result)
                 pbar.update(1)
 
+                if len(data) >= max_items_per_file:
+                    with open(f"{idx}|{base_filename}", "wb") as f:
+                        pickle.dump(data, f)
+                    data, idx = [], idx + 1
+                    logger.info(f"Exported to file: {f'{idx}|{base_filename}'}")
+
     logger.info(f"Got {len(data)} points!")
 
-    with open(filename, "wb") as f:
+    with open(f"{idx}|{base_filename}", "wb") as f:
         pickle.dump(data, f)
+
+    logger.info(f"Exported to file: {f'{idx}|{base_filename}'}")
 
 
 if __name__ == "__main__":
