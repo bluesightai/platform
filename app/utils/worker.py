af6f4f443043162776b0629b446a63a34da69c51
Alexander Smirnov <alex@asmirnov.xyz>
2024-07-08 20:56:15 +0100

    fix worker

--- app/utils/worker.py
+++ app/utils/worker.py
@@ -2,4 +2,4 @@ from uvicorn.workers import UvicornWorker
 
 
 class ProxyUvicornWorker(UvicornWorker):
-    CONFIG_KWARGS = {"proxy_headers": True}
+    CONFIG_KWARGS = {"proxy_headers": True, "forwarded_allow_ips": "*"}
