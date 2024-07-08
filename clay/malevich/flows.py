27f36331442e3b32e9cbc6ee63a9baf6d8dc4182
Alexander Smirnov <alex@asmirnov.xyz>
2024-07-08 14:44:03 +0100

    upd malevich deps

--- clay/malevich/flows.py
+++ clay/malevich/flows.py
@@ -1,5 +1,6 @@
-from malevich import Core, flow
-from malevich.clay import get_malevich_mock_data, inference
+# from malevich import Core, flow
+from malevich import Space, SpaceInterpreter, collection, flow, table
+from malevich.clay import get_malevich_mock_data, inference, mock_inference
 
 
 @flow(reverse_id="inference")
@@ -9,13 +10,50 @@ def inference_flow():
     return embeddings
 
 
-task = Core(
-    inference_flow,
-    core_host="https://core.malevich.ai/",
-    user="c148403e41894c74a2d83f0703d1223c",
-    access_key="40f191df-d8ce-483a-a5ce-8a654c1f19bd",
-)
-task.prepare()
-task.run()
-print(task.results()[0].get_df())
-task.stop()
+@flow
+def inference_flow():
+    satelite_images = collection(
+        "clay_satilite_images",
+        # df=table(...),   # TODO: Define the input structure
+        df=table([1, 2, 3], columns=["test"]),
+        alias="input_images",
+    )
+
+    # TODO: Implement and replace the mock
+    # embeddings = inference()
+
+    embeddings = mock_inference(satelite_images)
+
+    # TODO: Save embeddings with Qdrant
+    return embeddings
+
+
+# task = Core(
+#     inference_flow,
+#     core_host="https://core.malevich.ai/",
+#     user="c148403e41894c74a2d83f0703d1223c",
+#     access_key="40f191df-d8ce-483a-a5ce-8a654c1f19bd",
+# )
+
+
+space_interpreter = SpaceInterpreter()
+
+task = inference_flow()
+
+task.interpret(space_interpreter)
+
+task.get_interpreted_task().upload()
+
+
+# space_interpreter.interpret(task)
+
+# task = Space(inference_flow, reverse_id="inference_flow")
+
+# task.configure()
+# task.prepare()
+# task.run()
+# print(task.results()[0].get_df())
+
+# task.upload()
+
+# task.stop()
