27861f608e1177a54695fd5df058299074e051de
Alexander Smirnov <alex@asmirnov.xyz>
2024-07-16 00:59:59 +0100

    fix gpu version

--- clay/utils.py
+++ clay/utils.py
@@ -126,9 +126,11 @@ def posemb_sincos_2d(h, w, dim, temperature: int = 10000, dtype=torch.float32):
 
 def posemb_sincos_2d_with_gsd(h, w, dim, gsd=1.0, temperature: int = 10000, dtype=torch.float32):
     y, x = torch.meshgrid(torch.arange(h), torch.arange(w), indexing="ij")
+    y, x = y.to(device), x.to(device)
     assert (dim % 4) == 0, "feature dimension must be multiple of 4 for sincos emb"
 
     omega = torch.arange(dim // 4) / (dim // 4 - 1)
+    omega = omega.to(device)
     omega = 1.0 / (temperature ** (2 * omega / dim)) * (gsd / 1.0)  # Adjusted for g
 
     y = y.flatten()[:, None] * omega[None, :]
@@ -143,6 +145,7 @@ def posemb_sincos_1d(pos, dim, temperature: int = 10000, dtype=torch.float32):
 
     omega = torch.arange(dim // 2) / (dim // 2 - 1)
     omega = 1.0 / (temperature**omega)
+    omega = omega.to(device)
 
     scaled_pos = pos[:, None] * omega[None, :]
     pe = torch.cat((scaled_pos.sin(), scaled_pos.cos()), dim=1)
