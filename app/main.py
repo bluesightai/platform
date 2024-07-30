55ba1db700ed075c50453b463d56bcfccf6f9dc9
Alexander Smirnov <alex@asmirnov.xyz>
2024-07-30 02:10:56 +0200

    add api description

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ from app.api.api import api_router
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(servers=[{"url": "https://api.bluesight.ai/"}])
+    app = FastAPI(
+        servers=[{"url": "https://api.bluesight.ai/"}],
+        description="The Bluesight API enables you to create embeddings using the Clay foundation model and run training jobs on top of Clay.",
+    )
 
     app.add_middleware(
         CORSMiddleware,
@@ -15,7 +18,6 @@ def create_app() -> FastAPI:
         allow_headers=["*"],
     )
 
-    # Include the routers
     app.include_router(api_router)
 
     return app
