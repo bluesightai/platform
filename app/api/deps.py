3a9da80d114f98637065a146b60a7a7fbc0c6718
Alexander Smirnov <alex@asmirnov.xyz>
2024-08-04 14:05:39 +0200

    fix Supabase `super_client` types

--- app/api/deps.py
+++ app/api/deps.py
@@ -6,7 +6,7 @@ from supabase.client import AsyncClient, ClientOptions
 from app.config import config
 from supabase import acreate_client
 
-super_client: AsyncClient
+super_client: AsyncClient | None = None
 
 
 async def init_super_client() -> None:
@@ -18,7 +18,21 @@ async def init_super_client() -> None:
     )
 
 
+def get_super_client() -> AsyncClient:
+    if super_client is None:
+        raise RuntimeError("Supabase client is not initialized")
+    return super_client
+
+
 async def get_db() -> AsyncClient:
+    """
+    Later this will be dependent on the Token and we
+    will return a client based on the user's token
+    with the appropriate permissions
+    """
+
+    if super_client is None:
+        raise RuntimeError("Supabase client is not initialized")
     return super_client
 
 
