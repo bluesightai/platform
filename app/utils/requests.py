468f73a68af388c696a8aea008d89e89450108ea
Alexander Smirnov <alex@asmirnov.xyz>
2024-08-04 15:22:06 +0200

    make ip retrieval async

--- app/utils/requests.py
+++ app/utils/requests.py
@@ -8,26 +8,16 @@ from supabase.client import AsyncClient
 
 from app.config import config
 
-# async def fetch_ip_data(ip: str) -> Dict[str, Any]:
-#     url = f"http://ip-api.com/json/{ip}"
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url) as response:
-#             if response.status == 200:
-#                 data = await response.json()
-#             else:
-#                 data = {"query": ip}
-#                 print(f"Request failed with status code: {response.status}")
-#     return data
 
-
-def fetch_ip_data(ip: str) -> Dict[str, Any]:
+async def fetch_ip_data(ip: str) -> Dict[str, Any]:
     url = f"http://ip-api.com/json/{ip}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-    else:
-        data = {"query": ip}
-        logger.error(f"IP request failed with status code: {response.status_code} and response: {response.text}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+            else:
+                data = {"query": ip}
+                print(f"IP request failed with status code: {response.status} and response: {response.text}")
     return data
 
 
