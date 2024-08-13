bff86da47d5f8eefb01771492de9ae26a7c58f29
Alexander Smirnov <alex@asmirnov.xyz>
2024-08-13 16:26:23 +0100

    apply formatting

--- scripts/vector_geo_utils/db.py
+++ scripts/vector_geo_utils/db.py
@@ -1,12 +1,14 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 # Load the .env file
 load_dotenv()
 
 # Get the password from the .env file
-password = os.getenv('PASSWORD')
+password = os.getenv("SUPABASE_POSTGRES_PASSWORD")
 
 
-postgres_uri = f"postgresql://postgres.biccczfztgnfaqzmizan:{password}@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
-
+postgres_uri = (
+    f"postgresql://postgres.biccczfztgnfaqzmizan:{password}@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+)
