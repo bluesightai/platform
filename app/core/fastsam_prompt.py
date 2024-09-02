c4564ad0e2e09bc41f5a692eea9829eb6a6849b5
Alexander Smirnov <alex@asmirnov.xyz>
2024-09-02 16:05:18 -0300

    crop out whole object instead of highlighting it

--- app/core/fastsam_prompt.py
+++ app/core/fastsam_prompt.py
@@ -54,17 +54,14 @@ class FastSAMPrompt:
             image_array = np.array(image)
         else:
             image_array = image
-        segmented_image_array = np.zeros_like(image_array)
+
         x1, y1, x2, y2 = bbox
-        segmented_image_array[y1:y2, x1:x2] = image_array[y1:y2, x1:x2]
-        segmented_image = Image.fromarray(segmented_image_array)
-        black_image = Image.new("RGB", image.size, (255, 255, 255))
-        # transparency_mask = np.zeros_like((), dtype=np.uint8)
-        transparency_mask = np.zeros((image_array.shape[0], image_array.shape[1]), dtype=np.uint8)
-        transparency_mask[y1:y2, x1:x2] = 255
-        transparency_mask_image = Image.fromarray(transparency_mask, mode="L")
-        black_image.paste(segmented_image, mask=transparency_mask_image)
-        return black_image
+        cropped_image_array = image_array[y1:y2, x1:x2]
+
+        # Convert the cropped array back to an Image
+        cropped_image = Image.fromarray(cropped_image_array)
+
+        return cropped_image
 
     def _format_results(self, result, filter=0):
         annotations = []
